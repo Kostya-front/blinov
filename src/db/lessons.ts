6f89786874ef2da0a6a5775a49b492e66a2fe03a
Kens <kostya.pasechnik@inbox.ru>
2024-08-02 06:45:43 +0300

    add new lesson

--- src/db/lessons.ts
+++ src/db/lessons.ts
@@ -1,4 +1,5 @@
 export const lessons = [
   {id: 1, date: '30 июля (вторник)', duration: 60, topic: 'Разбор useReducer', link: 'https://disk.yandex.ru/i/KaACm6_5K_UyKQ'},
-  {id: 1, date: '1 августа (четверг)', duration: 60, topic: 'Разбор задачи на useReducer', link: 'https://disk.yandex.ru/i/Q9x5i2r8WecjUw'}
+  {id: 2, date: '1 августа (четверг)', duration: 60, topic: 'Разбор задачи на useReducer', link: 'https://disk.yandex.ru/i/Q9x5i2r8WecjUw'},
+  {id: 3, date: '2 августа (пятница)', duration: 60, topic: 'Разбор задачи на useReducer', link: 'https://disk.yandex.ru/d/4OatTrBKdSFp3Q'},
 ]
