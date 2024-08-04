568b0878fbb607feaad9280a7033d8f1e7202517
Kens <kostya.pasechnik@inbox.ru>
2024-08-04 09:20:48 +0300

    add new topic into db

--- src/db/topics.ts
+++ src/db/topics.ts
@@ -1,8 +1,9 @@
 import { filter } from "@/db/var/filter";
 import { map } from "@/db/var/map";
-
+import { destructurization } from "@/db/var/destructurization";
 
 export const topics = [
   {id:1, title: 'Метод filter()', description: filter},
   {id:2, title: 'Метод map()', description: map},
+  {id:3, title: 'Деструктуризация (...)', description: destructurization},
 ]
