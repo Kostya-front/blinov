1a26a5a04d1e9ec1c11a334a4a75c4fd222b953b
Kens <kostya.pasechnik@inbox.ru>
2024-08-04 09:09:40 +0300

    add new lesson

--- src/db/lessons.ts
+++ src/db/lessons.ts
@@ -2,4 +2,5 @@ export const lessons = [
   {id: 1, date: '30 июля (вторник)', duration: 60, topic: 'Разбор useReducer', link: 'https://disk.yandex.ru/i/KaACm6_5K_UyKQ'},
   {id: 2, date: '1 августа (четверг)', duration: 60, topic: 'Разбор задачи на useReducer', link: 'https://disk.yandex.ru/i/Q9x5i2r8WecjUw'},
   {id: 3, date: '2 августа (пятница)', duration: 60, topic: 'Разбор задачи на useReducer', link: 'https://disk.yandex.ru/d/4OatTrBKdSFp3Q'},
+  {id: 4, date: '4 августа (суббота)', duration: 120, topic: 'Победа на задачей про useReducer', link: 'https://disk.yandex.ru/i/YoW0SJLAK12cwA'},
 ]
