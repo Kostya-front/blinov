2377e9f97fce61656c8ad3e81d35498ab2859233
Kens <kostya.pasechnik@inbox.ru>
2024-08-06 06:41:34 +0300

    add new lesson

--- src/db/lessons.ts
+++ src/db/lessons.ts
@@ -3,5 +3,6 @@ export const lessons = [
   {id: 2, date: '1 августа (четверг)', duration: 60, topic: 'Разбор задачи на useReducer', link: 'https://disk.yandex.ru/i/Q9x5i2r8WecjUw'},
   {id: 3, date: '2 августа (пятница)', duration: 60, topic: 'Разбор задачи на useReducer', link: 'https://disk.yandex.ru/d/4OatTrBKdSFp3Q'},
   {id: 4, date: '4 августа (воскресенье)', duration: 120, topic: 'Победа на задачей про useReducer', link: 'https://disk.yandex.ru/i/YoW0SJLAK12cwA'},
-  {id: 4, date: '5 августа (понедельник)', duration: 60, topic: 'Изучение сервисов', link: 'https://disk.yandex.ru/d/2nXXeX42U2sLdw'},
+  {id: 5, date: '5 августа (понедельник)', duration: 60, topic: 'Изучение сервисов', link: 'https://disk.yandex.ru/d/2nXXeX42U2sLdw'},
+  {id: 6, date: '6 августа (вторник)', duration: 60, topic: 'Разбор видимости var,const, let. Знакомство с redux', link: 'https://disk.yandex.ru/d/1I5WsmO-QcN4aQ'},
 ]
