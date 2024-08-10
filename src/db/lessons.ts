7274f7bf04b02a817d5163864e0601364f80ba3e
Kens <kostya.pasechnik@inbox.ru>
2024-08-10 09:44:40 +0300

    add new lesson without link

--- src/db/lessons.ts
+++ src/db/lessons.ts
@@ -5,7 +5,8 @@ export const lessons = [
   {id: 4, date: '4 августа (воскресенье)', duration: 120, topic: 'Победа на задачей про useReducer', link: 'https://disk.yandex.ru/i/YoW0SJLAK12cwA'},
   {id: 5, date: '5 августа (понедельник)', duration: 60, topic: 'Изучение сервисов', link: 'https://disk.yandex.ru/d/2nXXeX42U2sLdw'},
   {id: 6, date: '6 августа (вторник)', duration: 60, topic: 'Разбор видимости var,const, let. Знакомство с redux', link: 'https://disk.yandex.ru/d/1I5WsmO-QcN4aQ'},
-  {id: 6, date: '9 августа (пятница)', duration: 60, topic: 'Разбор Redux Toolkit', link: 'https://disk.yandex.ru/d/h1febMbOjIm3QQ'},
+  {id: 7, date: '9 августа (пятница)', duration: 60, topic: 'Разбор Redux Toolkit', link: 'https://disk.yandex.ru/d/h1febMbOjIm3QQ'},
+  {id: 8, date: '10 августа (суббота)', duration: 90, topic: 'Создание приложения "Счётчик" с Redux Toolkit', link: ''},
 ]
 
 // https://www.figma.com/design/aNxp3G306blFzHb8Cf7oib/Untitled?node-id=2-2&t=oL8j3rsRC5Fk9ARZ-0
