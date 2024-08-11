c73ea49fd5f216892a41d4563b4b24c12c90f444
Kens <kostya.pasechnik@inbox.ru>
2024-08-11 09:51:24 +0300

    add new lesson(11.08)

--- src/db/lessons.ts
+++ src/db/lessons.ts
@@ -7,6 +7,6 @@ export const lessons = [
   {id: 6, date: '6 августа (вторник)', duration: 60, topic: 'Разбор видимости var,const, let. Знакомство с redux', link: 'https://disk.yandex.ru/d/1I5WsmO-QcN4aQ'},
   {id: 7, date: '9 августа (пятница)', duration: 60, topic: 'Разбор Redux Toolkit', link: 'https://disk.yandex.ru/d/h1febMbOjIm3QQ'},
   {id: 8, date: '10 августа (суббота)', duration: 90, topic: 'Создание приложения "Счётчик" с Redux Toolkit', link: ''},
+  {id: 9, date: '11 августа (воскресенье)', duration: 150, topic: 'Решение задачи по Redux Toolkit в тренажере', link: 'https://disk.yandex.ru/i/T4YbvopVVJq_-Q'},
 ]
-
 // https://www.figma.com/design/aNxp3G306blFzHb8Cf7oib/Untitled?node-id=2-2&t=oL8j3rsRC5Fk9ARZ-0
