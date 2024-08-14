7bcf50b5cad78626c9501368217ca35caeecdf53
Kens <kostya.pasechnik@inbox.ru>
2024-08-14 11:58:56 +0300

    add new topics (14.08)

--- src/db/topics.ts
+++ src/db/topics.ts
@@ -2,10 +2,14 @@ import { filter } from "@/db/var/filter";
 import { map } from "@/db/var/map";
 import { destructurization } from "@/db/var/destructurization";
 import { rtk } from "./var/rtk";
+import { useSelectorDescription } from "./var/useSelector";
+import { useDispatchDescription } from "./var/useDispatch";
 
 export const topics = [
   {id:1, title: 'Метод filter()', description: filter},
   {id:2, title: 'Метод map()', description: map},
   {id:3, title: 'Деструктуризация (...)', description: destructurization},
   {id:4, title: 'Redux Toolkit', description: rtk},
+  {id:5, title: 'useSelector', description: useSelectorDescription},
+  {id:6, title: 'useDispatch', description: useDispatchDescription},
 ]
