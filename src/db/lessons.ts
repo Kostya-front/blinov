7b62b7328ef6d5f18578237350daa89f32ac6795
Kens <kostya.pasechnik@inbox.ru>
2024-08-17 06:17:48 +0300

    add new lesson(routing)

--- src/db/lessons.ts
+++ src/db/lessons.ts
@@ -11,5 +11,6 @@ export const lessons = [
   {id: 10, date: '13 августа (вторник)', duration: 60, topic: 'Знакомство с thunks', link: 'https://disk.yandex.ru/d/m4TEDbk8kdOxCw'},
   {id: 11, date: '14 августа (среда)', duration: 60, topic: 'Более подбробное изучение thunks', link: 'https://disk.yandex.ru/i/yQy_4FWGs1p0Qg'},
   {id: 12, date: '15 августа (четверг)', duration: 60, topic: 'Решение задачи по thunks', link: 'https://disk.yandex.ru/i/0u0adG6GxKuJww'},
+  {id: 13, date: '16 августа (пятница)', duration: 60, topic: 'Знакомства с роутингом', link: 'https://disk.yandex.ru/d/oRaYTl0K9XvH8w'},
 ]
 // https://www.figma.com/design/aNxp3G306blFzHb8Cf7oib/Untitled?node-id=2-2&t=oL8j3rsRC5Fk9ARZ-0
