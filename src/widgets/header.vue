77aafa6086748533bd8b3a8828dfbc807cceb4af
Kens <kostya.pasechnik@inbox.ru>
2024-09-01 06:38:04 +0300

    add new link

--- src/widgets/header.vue
+++ src/widgets/header.vue
@@ -1,17 +1,19 @@
 <template>
   <header :class="$style.header">
-    <a target="_blank" href="https://telemost.yandex.ru/j/30690440236063">
-      <img src="https://dynamic.design.com/preview/logodraft/bb05106d-e831-4746-bcdc-5bb87b72250b/image/large.png" alt="logo_link"/>
+    <a href="https://telemost.yandex.ru/j/14636994093138" target="_blank">
+      <img alt="logo_link"
+           src="https://dynamic.design.com/preview/logodraft/bb05106d-e831-4746-bcdc-5bb87b72250b/image/large.png" />
     </a>
 
-    <ToggleMenu/>
+    <ToggleMenu />
   </header>
 </template>
 
 <script>
 import ToggleMenu from "@/features/menu/ToggleMenu.vue";
+
 export default {
-  name: 'header',
+  name: "header",
   components: { ToggleMenu }
 };
 </script>
